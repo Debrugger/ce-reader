f91e0380ce207183fb093446c31fa416cb01cacd
Thomas <tom@boulderblock.eu>
2018-01-29 20:39:55 +0100

    separate sort func

--- compress.c
+++ compress.c
@@ -51,6 +51,68 @@ void freq_list_init(freq_list* p)
 	p->freq = p->c = 0;
 }
 
+freq_list* insertion_sort(freq_list* unsorted_head)
+{
+	freq_list* sorted_head;
+
+	sorted_head = unsorted_head;
+	unsorted_head = unsorted_head->next;
+	sorted_head->next = NULL;
+	int i = 0;
+	while (unsorted_head)
+	{
+		//Sonderfall: in sorted list nur head
+		//freqhead next zwischenspeichern
+		//wenn head groesser als freqhead
+		//freqhead next = head, head = freqhead
+		//else head kleiner als freqhead
+		//head next = freqhead, freqhead next = null
+		freq_list* unsorted_next = unsorted_head->next;
+		
+		if (!sorted_head->next)
+		{
+			if (sorted_head->freq > unsorted_head->freq)
+			{
+				unsorted_head->next = sorted_head;
+				sorted_head = unsorted_head;
+			}
+			else if (sorted_head->freq <= unsorted_head->freq)
+			{
+				sorted_head->next = unsorted_head;
+				unsorted_head->next = NULL;
+			}
+		}
+
+		//wir nehmen freq_head, gehen durch sortierte list bis das naechste groesser als freq_head oder current.next == null
+		//last_smaller = das letzte davor
+		//freqhead next auf last_smaller->next
+		//last_smaller next auf freqhead
+		//freqhead auf freqhead next
+		else
+		{         
+		  	freq_list* sorted_cur = sorted_head;
+			while (sorted_cur->next)
+			{
+				if (unsorted_head->freq < sorted_cur->next->freq)
+					break;
+				sorted_cur = sorted_cur->next;
+			}
+			if (!sorted_cur->next)
+			{
+				sorted_cur->next = unsorted_head;
+				unsorted_head->next = NULL;
+			}
+			else
+			{
+				unsorted_head->next = sorted_cur->next;
+				sorted_cur->next = unsorted_head;
+			}
+		}
+		unsorted_head = unsorted_next;
+	}
+	return sorted_head;
+}
+
 int main(int argc, char* argv[])
 {
 	int16_t occurrences[255];
@@ -88,63 +150,11 @@ int main(int argc, char* argv[])
 
 	//for (freq_list* p = freq_head; p->next; p = p->next)
 	  // printf("%d ", p->freq);
+	
+	freq_list* sorted_freq_head = insertion_sort(freq_head);
 
-	freq_list* up;
-	sorted_freq_head = freq_head;
-	freq_head = freq_head->next;
-	sorted_freq_head->next = NULL;
-	int i = 0;
-	while (freq_head)
-	{
-		//Sonderfall: in sorted list nur head
-		//freqhead next zwischenspeichern
-		//wenn head groesser als freqhead
-		//freqhead next = head, head = freqhead
-		//else head kleiner als freqhead
-		//head next = freqhead, freqhead next = null
-		freq_list* unsorted_next = freq_head->next;
-		
-		if (!sorted_freq_head->next)
-		{
-			if (sorted_freq_head->freq > freq_head->freq)
-			{
-				freq_head->next = sorted_freq_head;
-				sorted_freq_head = freq_head;
-			}
-			else if (sorted_freq_head->freq <= freq_head->freq)
-			{
-				sorted_freq_head->next = freq_head;
-				freq_head->next = NULL;
-			}
-		}
-
-		//wir nehmen freq_head, gehen durch sortierte list bis das naechste groesser als freq_head oder current.next == null
-		//last_smaller = das letzte davor
-		//freqhead next auf last_smaller->next
-		//last_smaller next auf freqhead
-		//freqhead auf freqhead next
-		else
-		{         
-		  	freq_list* sorted_cur = sorted_freq_head;
-			while (sorted_cur->next)
-			{
-				if (freq_head->freq < sorted_cur->next->freq)
-					break;
-				sorted_cur = sorted_cur->next;
-			}
-			if (!sorted_cur->next)
-			{
-				sorted_cur->next = freq_head;
-				freq_head->next = NULL;
-			}
-			else
-			{
-				freq_head->next = sorted_cur->next;
-				sorted_cur->next = freq_head;
-			}
-		}
-		freq_head = unsorted_next;
-	}
 	for (freq_list* p = sorted_freq_head; p; p = p->next)
-		printf("%d", p->freq);
+		printf("%d ", p->freq);
+
+	return 0;
 }
